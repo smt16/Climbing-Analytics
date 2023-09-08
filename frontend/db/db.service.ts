c8ae6fe7a6db2e668da212eba2e7a276931ee47b
smt16 <stoguri16@gmail.com>
2023-09-08 16:24:19 +0100

    database set up

---
+++ frontend/db/db.service.ts
@@ -0,0 +1,30 @@
+import { Db, MongoClient } from 'mongodb';
+
+// All DB collections for collection arg types
+type collection = 
+  'users'
+
+const client = new MongoClient(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@climbinganalytics.ihhngvy.mongodb.net/?retryWrites=true&w=majority`);
+let db: Db; 
+
+async function connect() {
+  await client.connect();
+  db = client.db('ClimbingAnalytics');
+}
+
+/**
+ * Get one row from the DB
+ */
+export async function getOne(collection: collection, query: Record<string, any>) {
+  try {
+    await connect();
+
+    const coll = db.collection(collection);
+    
+    return coll.findOne(query);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
